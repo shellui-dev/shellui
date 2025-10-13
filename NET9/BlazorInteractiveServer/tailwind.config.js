aa666bee2e1087c46c938bf6d7c0988b591fba71
Shewart <shewartshepherd23@gmail.com>
2025-10-14 00:16:41 +0200

    feat: Integrate Tailwind CSS configuration and update theme toggle component in BlazorInteractiveServer

---
+++ NET9/BlazorInteractiveServer/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './Components/**/*.{razor,html,cshtml}',
+    './Pages/**/*.{razor,html,cshtml}',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+}
