35564f5ee03df745859fdb511cc455998ec2157d
Shewart <shewartshepherd23@gmail.com>
2026-01-20 22:48:59 +0200

    feat: add charts demo and data models

---
+++ NET9/BlazorInteractiveServer/Components/Models/ChartModels.cs
@@ -0,0 +1,13 @@
+namespace BlazorInteractiveServer.Components.Models;
+
+public class SalesData
+{
+    public string Month { get; set; } = "";
+    public decimal Amount { get; set; }
+}
+
+public class CategoryData
+{
+    public string Category { get; set; } = "";
+    public decimal Value { get; set; }
+}
