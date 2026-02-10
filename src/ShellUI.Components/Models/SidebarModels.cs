c74c88120bf36ce2cfb9f73fdcbf409462358ea8
Shewart <shewartshepherd23@gmail.com>
2026-02-11 01:19:40 +0200

    feat: add Sidebar models and JavaScript interop for enhanced sidebar functionality

---
+++ src/ShellUI.Components/Models/SidebarModels.cs
@@ -0,0 +1,21 @@
+namespace ShellUI.Components;
+
+public enum SidebarVariant
+{
+    Sidebar,
+    Floating,
+    Inset
+}
+
+public enum SidebarCollapsible
+{
+    Offcanvas,
+    Icon,
+    None
+}
+
+public enum SidebarSide
+{
+    Left,
+    Right
+}
